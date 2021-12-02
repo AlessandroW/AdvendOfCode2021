310a6ae5855a17ecd0cb136d29dfccbec0d7f1bf
Alessandro Wollek <alessandro.wollek@tum.de>
2021-12-02 19:32:00 +0100

    Day 2 in Python

---
+++ day2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+with open("day2.txt") as f:
+    input = f.read().split("\n")
+
+horizontal = 0
+depth = 0
+depth = 0
+aim = 0
+
+for line in input[:-1]:
+    value = int(line.split()[-1])
+    if "forward" in line:
+        horizontal += value
+        depth += aim * value
+    elif "down" in line:
+        aim += value
+    elif "up" in line:
+        aim -= value
+print("Part 1")
+print(horizontal * aim)
+print("Part 2")
+print(horizontal * depth)
