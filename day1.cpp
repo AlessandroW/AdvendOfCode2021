d4840b2109e76716047138880302cd843c7673bf
Alessandro Wollek <alessandro.wollek@tum.de>
2021-12-03 18:29:13 +0100

    format

--- day1.cpp
+++ day1.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <queue>
 
-int main(int argc, char **argv){
-std::ifstream infile("day1.txt");
+int main(int argc, char **argv) {
+  std::ifstream infile("day1.txt");
 
-int result = 0;
-int i = 0;
-int currentNumber;
-std::queue<int> prevNumbers {};
+  int result = 0;
+  int i = 0;
+  int currentNumber;
+  std::queue<int> prevNumbers{};
 
-
-while (infile >> currentNumber)
-{
+  while (infile >> currentNumber) {
     prevNumbers.push(currentNumber);
     ++i;
-    if (i >= 4){
-        if(currentNumber > prevNumbers.front()) ++result;
-        prevNumbers.pop();
+    if (i >= 4) {
+      if (currentNumber > prevNumbers.front())
+        ++result;
+      prevNumbers.pop();
     }
-}
+  }
 
   std::cout << result << '\n';
 }
