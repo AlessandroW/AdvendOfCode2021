7cd64da0746110aa6ede912a508a7cd22e592736
Alessandro Wollek <a@wollek.dev>
2022-07-19 18:17:54 +0200

    refactor: clean up

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "day1.h"
+#include "day2.h"
 
 using namespace std;
 
 int main() {
 
-    auto day = Day1();
-    day.print_solution_1();
-    day.print_solution_2();
+    auto day = Day2();
+    // day.print_solution_1();
+    // day.print_solution_2();
     return 0;
 }
